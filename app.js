8748efb5cc67bb7b7411d83c6c55a845da41503b
happygod119 <happ0983493655@gmail.com>
2022-01-25 18:00:13 +0800

    feat: add mongoose & method-override

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+//- 載入 method-override
+const methodOverride = require("method-override");
+require("./config/mongoose"); //- 引用mongoose;
 
-
-
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+const routes = require("./routes"); //- 引用路由器
+app.use(methodOverride("_method")); //- 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(routes); //- 將 request 導入路由器
 
 // 設定port
 app.listen(port, () => {
