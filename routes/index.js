66cb07dcf917d6da9e8a085ad021d8eb594e5969
happygod119 <happ0983493655@gmail.com>
2022-01-25 22:32:49 +0800

    feat:URL shorten Success

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const home = require("./modules/home");
+const shortUrl = require('./modules/shortUrl')
 
 
 router.use("/", home);
-
+router.use("/urls", shortUrl);
 
 module.exports = router;
