2b173e812aa0893bc976983bfa52167290dba802
happygod119 <happ0983493655@gmail.com>
2022-01-25 18:34:24 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// Express 環境
+//* Express 環境
 const express = require("express");
 const app = express();
+//* 載入handlebars
+const exphbs = require("express-handlebars");
 
 //- 載入 method-override
 const methodOverride = require("method-override");
 require("./config/mongoose"); //- 引用mongoose;
 
 const routes = require("./routes"); //- 引用路由器
+
+//* 設定使用handlebars
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
 app.use(methodOverride("_method")); //- 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(routes); //- 將 request 導入路由器
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-res.send("hello world");
+  res.render("index");
 });
 module.exports = router;
